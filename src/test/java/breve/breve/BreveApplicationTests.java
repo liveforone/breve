da24fa8c510ee3da5262faade2cef1389206d46b
liveforone <yc4852@gmail.com>
2022-11-04 22:08:49 +0900

    Initial commit

---
+++ src/test/java/breve/breve/BreveApplicationTests.java
@@ -0,0 +1,13 @@
+package breve.breve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
